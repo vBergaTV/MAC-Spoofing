7183c247c9ae1bdfbeb109b51d5030d24b96a3fb
vBergaTV <79266773+vBergaTV@users.noreply.github.com>
2021-02-18 09:41:59 -0500

    Delete mac_Spoofing_wip.py

--- mac_Spoofing_wip.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-subprocess.call("ifconfig enp2s0 down", shell=True)
-subprocess.call("ifconfig enp2s0 hw ether 00:2a:01:bb:22:cc", shell=True)
-subprocess.call("ifconfig enp2s0 up", shell=True)
